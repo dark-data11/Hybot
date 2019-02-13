9ea4c100f871b033748a82e2a7446bb0a8276537
Lewis Crichton <lewistehminerz@outlook.com>
2019-02-13 16:05:13 +0000

    refactor(commands/prefix.js): remove permissions check in execute
    no longer needed by previous commit

--- commands/prefix.js
+++ commands/prefix.js
@@ -7,34 +7,26 @@ module.exports = class Prefix extends Command {
 		this.name = 'prefix';
 		this.description = "Sets this guild's prefix.";
 		this.permissionsRequired = {
-			user: [
-				'administrator'
-			],
+			user: ['administrator'],
 			bot: []
-		}
+		};
 	}
 
 	async execute({msg, args, db}) {
-		if (msg.member.permission.has('manageGuild')) {
-			let requestedPrefix = args.join(' ');
+		let requestedPrefix = args.join(' ');
 
-			if (requestedPrefix === '')
-				return await msg.channel.createMessage('You must type a prefix!');
+		if (requestedPrefix === '')
+			return await msg.channel.createMessage('You must type a prefix!');
 
-			let collection = db.collection('guild');
+		let collection = db.collection('guild');
 
-			await collection.updateOne(
-				{guildId: msg.channel.guild.id},
-				{$set: {prefix: requestedPrefix}}
-			);
+		await collection.updateOne(
+			{ guildId: msg.channel.guild.id },
+			{ $set: { prefix: requestedPrefix } }
+		);
 
-			await msg.channel.createMessage(
-				'Prefix has been updated to `' + requestedPrefix + '`.'
-			);
-		} else {
-			await msg.channel.createMessage(
-				'You do not have the required permissions!'
-			);
-		}
+		await msg.channel.createMessage(
+			'Prefix has been updated to `' + requestedPrefix + '`.'
+		);
 	}
 };
