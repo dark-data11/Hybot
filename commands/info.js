7711cc2bd8bda52f22c9924e50ddfbb345060653
Mary <ipadlover8322@gmail.com>
2019-02-22 14:06:58 -0500

    refactor(info): Change message

--- commands/info.js
+++ commands/info.js
@@ -14,7 +14,7 @@ module.exports = class Info extends Command {
 			embed: {
 				title: 'Information',
 				description:
-					'HytaleBot is a Discord bot developed by HytaleHub. It provides utilities and Hytale-specific commands.'
+					'HytaleBot is a Discord bot developed by HytaleHub to provide all the users with a plenty full of features for the discord server owners to use and customize/manage their server and to get news for all the latest Hytale Blog posts.'
 			}
 		});
 	}
