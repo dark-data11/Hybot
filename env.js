c6e1be353287cdeace2cd3112a09c8dfb591e825
Mary <ipadlover8322@gmail.com>
2019-02-23 10:17:39 -0500

    perf(index): Build natively

---
+++ env.js
@@ -0,0 +1,12 @@
+module.exports = {
+	token: process.env.TOKEN,
+	logo: process.env.LOGO.split('\n'),
+	mongodb: {
+		host: process.env.MONGO_HOST,
+		port: process.env.MONGO_PORT,
+		db: process.env.MONGO_DB
+	},
+	theme: process.env.THEME,
+	prefix: 'h!' || process.env.PREFIX,
+	developers: ['96269247411400704', '196769986071625728']
+};
